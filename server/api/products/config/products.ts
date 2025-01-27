8689bca317a0756df7f6cf8b21d86da2c69f10e2
DESKTOP-6CDM0P7\sobol <sobol4156@gmail.com>
2025-01-27 21:15:44 +0300

    feat: more content on the market

--- server/api/products/config/products.ts
+++ server/api/products/config/products.ts
@@ -10,7 +10,7 @@ export const MOCK_PRODUCTS: Product[] = [{
   oldPrice: 190,
   newPrice: 149,
   rating: 3
-},{
+}, {
   id: 2,
   images: [{
     src: "/knifes/karambit-fade.png",
@@ -20,7 +20,7 @@ export const MOCK_PRODUCTS: Product[] = [{
   oldPrice: 250,
   newPrice: 189,
   rating: 5
-},{
+}, {
   id: 3,
   images: [{
     src: "/knifes/falchion-knife-fade.png",
@@ -30,7 +30,7 @@ export const MOCK_PRODUCTS: Product[] = [{
   oldPrice: 210,
   newPrice: 199,
   rating: 5
-},{
+}, {
   id: 4,
   images: [{
     src: "/knifes/shadow-daggers.png",
@@ -40,7 +40,7 @@ export const MOCK_PRODUCTS: Product[] = [{
   oldPrice: 159,
   newPrice: 149,
   rating: 4
-},{
+}, {
   id: 5,
   images: [{
     src: "/knifes/shadow-daggers-gold.png",
@@ -50,7 +50,7 @@ export const MOCK_PRODUCTS: Product[] = [{
   oldPrice: 220,
   newPrice: 209,
   rating: 5
-},{
+}, {
   id: 6,
   images: [{
     src: "/knifes/huntsman-knife-fade.png",
@@ -60,7 +60,7 @@ export const MOCK_PRODUCTS: Product[] = [{
   oldPrice: 149,
   newPrice: 135,
   rating: 2
-},{
+}, {
   id: 7,
   images: [{
     src: "/knifes/mp9-bayonet-blue.png",
@@ -70,7 +70,7 @@ export const MOCK_PRODUCTS: Product[] = [{
   oldPrice: 199,
   newPrice: 189,
   rating: 3
-},{
+}, {
   id: 8,
   images: [{
     src: "/knifes/butterfly-knife-fade.png",
@@ -80,7 +80,7 @@ export const MOCK_PRODUCTS: Product[] = [{
   oldPrice: 350,
   newPrice: 329,
   rating: 5
-},{
+}, {
   id: 9,
   images: [{
     src: "/knifes/karambit-black.png",
@@ -90,7 +90,7 @@ export const MOCK_PRODUCTS: Product[] = [{
   oldPrice: 140,
   newPrice: 129,
   rating: 1
-},{
+}, {
   id: 10,
   images: [{
     src: "/knifes/karambit-knife-red.png",
@@ -100,7 +100,7 @@ export const MOCK_PRODUCTS: Product[] = [{
   oldPrice: 290,
   newPrice: 279,
   rating: 4
-},{
+}, {
   id: 11,
   images: [{
     src: "/knifes/bowie-knife-fade.png",
@@ -110,7 +110,7 @@ export const MOCK_PRODUCTS: Product[] = [{
   oldPrice: 199,
   newPrice: 185,
   rating: 3
-},{
+}, {
   id: 12,
   images: [{
     src: "/knifes/skeleton-knife.png",
@@ -120,7 +120,7 @@ export const MOCK_PRODUCTS: Product[] = [{
   oldPrice: 249,
   newPrice: 230,
   rating: 4
-},{
+}, {
   id: 13,
   images: [{
     src: "/knifes/butterfly-knife-blue.png",
@@ -130,7 +130,7 @@ export const MOCK_PRODUCTS: Product[] = [{
   oldPrice: 169,
   newPrice: 150,
   rating: 2
-},{
+}, {
   id: 14,
   images: [{
     src: "/knifes/butterfly-knife-dark.png",
@@ -140,7 +140,7 @@ export const MOCK_PRODUCTS: Product[] = [{
   oldPrice: 249,
   newPrice: 229,
   rating: 5
-},{
+}, {
   id: 15,
   images: [{
     src: "/knifes/paracord-knife.png",
@@ -343,5 +343,525 @@ export const MOCK_PRODUCTS: Product[] = [{
   oldPrice: 40,
   newPrice: 35,
   rating: 3,
+},
+{
+  id: 31,
+  images: [
+    {
+      src: "/guns/m4a1s-chanticos-fire.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "M4A1-S Chantico's Fire",
+  oldPrice: 150,
+  newPrice: 140,
+  rating: 4,
+},
+{
+  id: 32,
+  images: [
+    {
+      src: "/knifes/butterfly-knife-lore.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "Butterfly Knife Lore",
+  oldPrice: 800,
+  newPrice: 750,
+  rating: 5,
+},
+{
+  id: 33,
+  images: [
+    {
+      src: "/guns/aug-akhabara.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "AUG Akihabara Accept",
+  oldPrice: 600,
+  newPrice: 580,
+  rating: 5,
+},
+{
+  id: 34,
+  images: [
+    {
+      src: "/guns/p250-muertos.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "P250 Muertos",
+  oldPrice: 80,
+  newPrice: 70,
+  rating: 3,
+},
+{
+  id: 35,
+  images: [
+    {
+      src: "/guns/tec-9-fubar.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "Tec-9 Fubar",
+  oldPrice: 30,
+  newPrice: 25,
+  rating: 2,
+},
+{
+  id: 36,
+  images: [
+    {
+      src: "/guns/mac-10-neon-rider.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "MAC-10 Neon Rider",
+  oldPrice: 45,
+  newPrice: 40,
+  rating: 4,
+},
+{
+  id: 37,
+  images: [
+    {
+      src: "/guns/galil-eco.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "Galil AR Eco",
+  oldPrice: 55,
+  newPrice: 50,
+  rating: 4,
+},
+{
+  id: 38,
+  images: [
+    {
+      src: "/guns/mag-7-justice.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "MAG-7 Justice",
+  oldPrice: 25,
+  newPrice: 20,
+  rating: 3,
+},
+{
+  id: 39,
+  images: [
+    {
+      src: "/guns/negev-power-loader.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "Negev Power Loader",
+  oldPrice: 40,
+  newPrice: 35,
+  rating: 2,
+},
+{
+  id: 40,
+  images: [
+    {
+      src: "/knifes/stiletto-knife-doppler.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "Stiletto Knife Doppler",
+  oldPrice: 500,
+  newPrice: 480,
+  rating: 5,
+},
+{
+  id: 41,
+  images: [
+    {
+      src: "/guns/xm1014-incinerator.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "XM1014 Incinerator",
+  oldPrice: 30,
+  newPrice: 28,
+  rating: 3,
+},
+{
+  id: 42,
+  images: [
+    {
+      src: "/guns/five-seven-violent-daimyo.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "Five-Seven Violent Daimyo",
+  oldPrice: 50,
+  newPrice: 45,
+  rating: 4,
+},
+{
+  id: 43,
+  images: [
+    {
+      src: "/guns/ssg08-blood-in-water.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "SSG 08 Blood in the Water",
+  oldPrice: 200,
+  newPrice: 180,
+  rating: 4,
+},
+{
+  id: 44,
+  images: [
+    {
+      src: "/knifes/navaja-knife-blue.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "Navaja Knife Blue Steel",
+  oldPrice: 300,
+  newPrice: 280,
+  rating: 4,
+},
+{
+  id: 45,
+  images: [
+    {
+      src: "/knifes/classic-knife-forest-ddpat.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "Classic Knife Forest DDPAT",
+  oldPrice: 400,
+  newPrice: 380,
+  rating: 4,
+},
+{
+  id: 46,
+  images: [
+    {
+      src: "/guns/ump45-blaze.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "UMP-45 Blaze",
+  oldPrice: 60,
+  newPrice: 50,
+  rating: 3,
+},
+{
+  id: 47,
+  images: [
+    {
+      src: "/guns/scar20-cyrex.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "SCAR-20 Cyrex",
+  oldPrice: 70,
+  newPrice: 65,
+  rating: 4,
+},
+{
+  id: 48,
+  images: [
+    {
+      src: "/guns/g3sg1-orange-crash.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "G3SG1 Orange Crash",
+  oldPrice: 20,
+  newPrice: 18,
+  rating: 2,
+},
+{
+  id: 49,
+  images: [
+    {
+      src: "/knifes/falchion-knife-marble-fade.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "Falchion Knife Marble Fade",
+  oldPrice: 600,
+  newPrice: 580,
+  rating: 5,
+},
+{
+  id: 50,
+  images: [
+    {
+      src: "/guns/p90-asiimov.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "P90 Asiimov",
+  oldPrice: 200,
+  newPrice: 180,
+  rating: 4,
+},
+{
+  id: 51,
+  images: [
+    {
+      src: "/guns/awp-redline.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "AWP Redline",
+  oldPrice: 250,
+  newPrice: 230,
+  rating: 4,
+},
+{
+  id: 52,
+  images: [
+    {
+      src: "/knifes/talon-knife-fade-2.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "Talon Knife Fade",
+  oldPrice: 800,
+  newPrice: 750,
+  rating: 5,
+},
+{
+  id: 53,
+  images: [
+    {
+      src: "/guns/famas-djinn.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "FAMAS Djinn",
+  oldPrice: 90,
+  newPrice: 80,
+  rating: 3,
+},
+{
+  id: 54,
+  images: [
+    {
+      src: "/guns/usp-s-kill-confirmed-2.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "USP-S Kill Confirmed",
+  oldPrice: 300,
+  newPrice: 280,
+  rating: 5,
+},
+{
+  id: 55,
+  images: [
+    {
+      src: "/guns/pp-bizon-judgement-of-anubis.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "PP-Bizon Judgement of Anubis",
+  oldPrice: 150,
+  newPrice: 140,
+  rating: 4,
+},
+{
+  id: 56,
+  images: [
+    {
+      src: "/guns/desert-eagle-hypnotic.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "Desert Eagle Hypnotic",
+  oldPrice: 90,
+  newPrice: 85,
+  rating: 4,
+},
+{
+  id: 57,
+  images: [
+    {
+      src: "/knifes/butterfly-knife-urban-mask.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "Butterfly Knife Urban Mask",
+  oldPrice: 650,
+  newPrice: 620,
+  rating: 5,
+},
+{
+  id: 58,
+  images: [
+    {
+      src: "/guns/dual-berettas-cobalt-quartz.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "Dual Berettas Cobalt Quartz",
+  oldPrice: 40,
+  newPrice: 35,
+  rating: 2,
+},
+{
+  id: 59,
+  images: [
+    {
+      src: "/guns/glock-18-wasteland-rebel.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "Glock-18 Wasteland Rebel",
+  oldPrice: 200,
+  newPrice: 190,
+  rating: 4,
+},
+{
+  id: 60,
+  images: [
+    {
+      src: "/knifes/karambit-doppler-phase-4.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "Karambit Doppler Phase 4",
+  oldPrice: 900,
+  newPrice: 870,
+  rating: 5,
+},
+{
+  id: 61,
+  images: [
+    {
+      src: "/guns/m4a4-howl.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "M4A4 Howl",
+  oldPrice: 1000,
+  newPrice: 950,
+  rating: 5,
+},
+{
+  id: 62,
+  images: [
+    {
+      src: "/guns/awp-medusa.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "AWP Medusa",
+  oldPrice: 1200,
+  newPrice: 1150,
+  rating: 5,
+},
+{
+  id: 63,
+  images: [
+    {
+      src: "/guns/sg553-integral.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "SG 553 Integral",
+  oldPrice: 100,
+  newPrice: 90,
+  rating: 3,
+},
+{
+  id: 64,
+  images: [
+    {
+      src: "/guns/mp5-sd-oxide-blaze.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "Desert Eagle Oxide Blaze",
+  oldPrice: 30,
+  newPrice: 25,
+  rating: 2,
+},
+{
+  id: 65,
+  images: [
+    {
+      src: "/knifes/stiletto-knife-crimson-web.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "Stiletto Knife Crimson Web",
+  oldPrice: 700,
+  newPrice: 680,
+  rating: 5,
+},
+{
+  id: 66,
+  images: [
+    {
+      src: "/guns/ssg08-dragonfire.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "SSG 08 Dragonfire",
+  oldPrice: 200,
+  newPrice: 180,
+  rating: 4,
+},
+{
+  id: 67,
+  images: [
+    {
+      src: "/guns/ak47-fire-serpent.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "AK-47 Fire Serpent",
+  oldPrice: 1500,
+  newPrice: 1450,
+  rating: 5,
+},
+{
+  id: 68,
+  images: [
+    {
+      src: "/guns/m249-magma.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "M249 Magma",
+  oldPrice: 50,
+  newPrice: 45,
+  rating: 3,
+},
+{
+  id: 69,
+  images: [
+    {
+      src: "/guns/mac-10-ultraviolet.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "MAC-10 Ultraviolet",
+  oldPrice: 45,
+  newPrice: 40,
+  rating: 2,
+},
+{
+  id: 70,
+  images: [
+    {
+      src: "/knifes/flip-knife-autotronic.png",
+      alt: "Картинка",
+    },
+  ],
+  title: "Flip Knife Autotronic",
+  oldPrice: 750,
+  newPrice: 720,
+  rating: 5,
 },
 ]

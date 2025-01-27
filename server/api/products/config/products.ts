5e48615be2beadbfe04ebf67b63b0b3d7b0f7d4b
DESKTOP-6CDM0P7\sobol <sobol4156@gmail.com>
2025-01-27 17:56:59 +0300

    feat: first part of content filling

--- server/api/products/config/products.ts
+++ server/api/products/config/products.ts
@@ -7,8 +7,148 @@ export const MOCK_PRODUCTS: Product[] = [{
     alt: "Картинка",
   }],
   title: 'Falchion Knife',
+  oldPrice: 190,
+  newPrice: 149,
+  rating: 3
+},{
+  id: 2,
+  images: [{
+    src: "/knifes/karambit-fade.png",
+    alt: "Картинка",
+  }],
+  title: 'Karambit Fade',
+  oldPrice: 250,
+  newPrice: 189,
+  rating: 5
+},{
+  id: 3,
+  images: [{
+    src: "/knifes/falchion-knife-fade.png",
+    alt: "Картинка",
+  }],
+  title: 'Flip Knife',
+  oldPrice: 210,
+  newPrice: 199,
+  rating: 5
+},{
+  id: 4,
+  images: [{
+    src: "/knifes/shadow-daggers.png",
+    alt: "Картинка",
+  }],
+  title: 'Shadow Daggers',
+  oldPrice: 159,
+  newPrice: 149,
+  rating: 4
+},{
+  id: 5,
+  images: [{
+    src: "/knifes/shadow-daggers-gold.png",
+    alt: "Картинка",
+  }],
+  title: 'Shadow Daggers Gold',
+  oldPrice: 220,
+  newPrice: 209,
+  rating: 5
+},{
+  id: 6,
+  images: [{
+    src: "/knifes/huntsman-knife-fade.png",
+    alt: "Картинка",
+  }],
+  title: 'Huntsman Knife Fade',
+  oldPrice: 149,
+  newPrice: 135,
+  rating: 2
+},{
+  id: 7,
+  images: [{
+    src: "/knifes/mp9-bayonet-blue.png",
+    alt: "Картинка",
+  }],
+  title: 'M9 Bayonet',
+  oldPrice: 199,
+  newPrice: 189,
+  rating: 3
+},{
+  id: 8,
+  images: [{
+    src: "/knifes/butterfly-knife-fade.png",
+    alt: "Картинка",
+  }],
+  title: 'Butterfly Knife Fade',
+  oldPrice: 350,
+  newPrice: 329,
+  rating: 5
+},{
+  id: 9,
+  images: [{
+    src: "/knifes/karambit-black.png",
+    alt: "Картинка",
+  }],
+  title: 'Karambit',
+  oldPrice: 140,
+  newPrice: 129,
+  rating: 1
+},{
+  id: 10,
+  images: [{
+    src: "https://steamuserimages-a.akamaihd.net/ugc/1931506893351769772/52CC7C2E9AA09EAE62004F073EF60875E9D95037/",
+    alt: "Картинка",
+  }],
+  title: 'Falchion Knife',
   oldPrice: 19,
   newPrice: 15,
   rating: 3
-}
+},{
+  id: 11,
+  images: [{
+    src: "https://steamuserimages-a.akamaihd.net/ugc/1931506893351769772/52CC7C2E9AA09EAE62004F073EF60875E9D95037/",
+    alt: "Картинка",
+  }],
+  title: 'Falchion Knife',
+  oldPrice: 19,
+  newPrice: 15,
+  rating: 3
+},{
+  id: 12,
+  images: [{
+    src: "https://steamuserimages-a.akamaihd.net/ugc/1931506893351769772/52CC7C2E9AA09EAE62004F073EF60875E9D95037/",
+    alt: "Картинка",
+  }],
+  title: 'Falchion Knife',
+  oldPrice: 19,
+  newPrice: 15,
+  rating: 3
+},{
+  id: 13,
+  images: [{
+    src: "https://steamuserimages-a.akamaihd.net/ugc/1931506893351769772/52CC7C2E9AA09EAE62004F073EF60875E9D95037/",
+    alt: "Картинка",
+  }],
+  title: 'Falchion Knife',
+  oldPrice: 19,
+  newPrice: 15,
+  rating: 3
+},{
+  id: 14,
+  images: [{
+    src: "https://steamuserimages-a.akamaihd.net/ugc/1931506893351769772/52CC7C2E9AA09EAE62004F073EF60875E9D95037/",
+    alt: "Картинка",
+  }],
+  title: 'Falchion Knife',
+  oldPrice: 19,
+  newPrice: 15,
+  rating: 3
+},{
+  id: 15,
+  images: [{
+    src: "https://steamuserimages-a.akamaihd.net/ugc/1931506893351769772/52CC7C2E9AA09EAE62004F073EF60875E9D95037/",
+    alt: "Картинка",
+  }],
+  title: 'Falchion Knife',
+  oldPrice: 19,
+  newPrice: 15,
+  rating: 3
+},
 ]
--- shared/ui/swipers/images-swiper/images-swiper.vue
+++ shared/ui/swipers/images-swiper/images-swiper.vue
@@ -1,22 +1,35 @@
 <template>
-    <div
-      v-for="(item, i) of images"
-      :key="i"
-      class="images-swiper__slide"
-      lazy="true"
-    >
-      <img
-        :src="item.src"
-        :alt="item.alt"
-        loading="lazy"
-      />
-    </div>
+  <div
+    v-for="(item, i) of images"
+    :key="i"
+    class="images-swiper__slide"
+    lazy="true"
+  >
+    <img
+      class="images-swiper__img"
+      :src="item.src"
+      :alt="item.alt"
+      loading="lazy"
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
-import type { Images } from '~/entities/product/model';
+import type { Images } from "~/entities/product/model";
 
 defineProps<{
-  images: Images[]
-}>()
+  images: Images[];
+}>();
 </script>
+
+<style scoped lang="scss">
+.images-swiper {
+  &__slide {
+    @apply flex justify-center items-center min-h-[300px] max-h-[300px];
+  }
+
+  &__img {
+    @apply max-h-[200px];
+  }
+}
+</style>

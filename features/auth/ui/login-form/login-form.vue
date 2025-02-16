8287b522a2e42b5af7f168f7e049b123a4714fd8
DESKTOP-6CDM0P7\sobol <sobol4156@gmail.com>
2025-02-16 14:29:48 +0300

    fix: login form for mobile

--- features/auth/ui/login-form/login-form.vue
+++ features/auth/ui/login-form/login-form.vue
@@ -51,8 +51,9 @@ const handlerSubmit = async (e: Event) => {
 
 <style scoped lang="scss">
 .login-form {
-  @apply max-w-[440px] mx-auto p-[24px] rounded-[12px] border-[1px] border-[--border-login];
+  @apply max-w-[440px] min-w-[400px] mx-auto p-[24px] rounded-[12px] border-[1px] border-[--border-login];
   box-shadow: 0 4px 10px rgba(0, 0, 0, 0.3);
+  @apply sm:w-full sm:min-w-full sm:max-w-full sm:mx-0 sm:p-0 sm:rounded-none sm:border-[0] sm:shadow-none;
 }
 .login-link {
   @apply text-[--link] decoration-[none] transition-colors duration-300;
--- features/auth/ui/register-form/register-form.vue
+++ features/auth/ui/register-form/register-form.vue
@@ -34,7 +34,7 @@ const handlerSubmit = async (e: Event) => {
 </script>
 
 <template>
-  <div class="login-form">
+  <div class="register-form">
     <form @submit="handlerSubmit">
       <Input
         label="Email"
@@ -79,9 +79,10 @@ const handlerSubmit = async (e: Event) => {
 </template>
 
 <style scoped lang="scss">
-.login-form {
-  @apply max-w-[440px] mx-auto p-[24px] rounded-[12px] border-[1px] border-[--border-login];
+.register-form {
+  @apply max-w-[440px] min-w-[400px] mx-auto p-[24px] rounded-[12px] border-[1px] border-[--border-login];
   box-shadow: 0 4px 10px rgba(0, 0, 0, 0.3);
+  @apply sm:w-full sm:min-w-full sm:max-w-full sm:mx-0 sm:p-0 sm:rounded-none sm:border-[0] sm:shadow-none;
 }
 .register-link {
   @apply text-[--link] decoration-[none] transition-colors duration-300;
